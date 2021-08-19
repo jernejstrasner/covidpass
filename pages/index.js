c6517cf42f91fab7fe490d2d84b913f424fbd289
Jernej Strasner <jernej.strasner@gmail.com>
2021-08-19 17:45:23 +0200

    Basic TailwindCSS setup

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div class="flex flex-col justify-center items-center h-screen font-sans">
       <Head>
         <title>COVIDPass</title>
         <meta name="description" content="Generate your Apple Wallet EU COVID Pass" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main class="bg-gray-50 font-bold rounded-lg border shadow-lg p-10 text-center">
+        <h1 class="text-4xl text-blue-500 font-medium">
           <a href="https://ec.europa.eu/info/live-work-travel-eu/coronavirus-response/safe-covid-19-vaccines-europeans/eu-digital-covid-certificate_en" target="_blank">EU Digital COVID Certificate</a>
         </h1>
 
-        <p className={styles.description}>
+        <p class="font-normal mt-5">
           Add your EU Digital COVID Certificate to Apple Wallet.<br/>
-          This website stores no data whatsoever and it's source code is <a href="https://github.com/jernejstrasner/covidpass" target="_blank">publicly available</a>.
+          This website stores no data whatsoever and it's source code is <a class="text-blue-500 hover:underline" href="https://github.com/jernejstrasner/covidpass" target="_blank">publicly available</a>.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
